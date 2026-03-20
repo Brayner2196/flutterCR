2f0e6041dc25872a3d807e1b5a45c8e0259c2dec
Brainer <brainer.sinning@aspsols.com>
2026-03-20 16:40:27 -0500

    configuracion inicial

---
+++ flutter_residential/README.md
@@ -0,0 +1,5 @@
+# flutter_residential
+
+A new Flutter project.
+
+## Getting Started
---
+++ flutter_residential/android/app/src/main/kotlin/com/flutter_residential/flutter_residential/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.flutter_residential.flutter_residential
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
