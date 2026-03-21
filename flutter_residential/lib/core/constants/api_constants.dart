b161157990163c7ce387d644d5d2b605d89d2cc2
Brainer <brainer.sinning@aspsols.com>
2026-03-21 14:32:01 -0500

    feat: agregar soporte para el plugin Flutter Secure Storage en plataformas Linux, macOS y Windows

--- flutter_residential/lib/core/constants/api_constants.dart
+++ flutter_residential/lib/core/constants/api_constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
-  static const String baseUrl = 'http://10.0.2.2:8080';
+  static const String baseUrl = 'http://localhost:8080';
 
   // Auth
   static const String login = '/auth/login';
